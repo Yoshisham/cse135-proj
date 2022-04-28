d5ba89bd92e699eed47cc52779adfe61aa092787
Joshue Aguirre <j8aguirr@ucsd.edu>
2022-04-27 17:10:38 -0700

    04/27

---
+++ py-hello-html.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+print("Cache-Control:no-cache")
+print("Content-Type:text/html\r\n\r\n")
+
+print("<html>")
+print("<head><title>Hello World Python</title></head>")
+print("<body>")
+print("<h1>Joshue - Hello World Python!</h1>")
+print("</body></html>")
