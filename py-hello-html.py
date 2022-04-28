210782410d10bb9397bef4cbd19a108e841ef8e4
Joshue Aguirre <j8aguirr@ucsd.edu>
2022-04-27 17:12:15 -0700

    04/27

--- py-hello-html.py
+++ py-hello-html.py
@@ -5,6 +5,7 @@ print("Content-Type:text/html\r\n\r\n")
 
 print("<html>")
 print("<head><title>Hello World Python</title></head>")
+print("$ip=$_SERVER['REMOTE_ADDR'];")
 print("<body>")
 print("<h1>Joshue - Hello World Python!</h1>")
 print("</body></html>")
